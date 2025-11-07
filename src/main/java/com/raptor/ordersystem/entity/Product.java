ab1468b41cdf9f51e086c64e66014fbbc5cd68e1
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-11-07 11:21:12 -0400

    feat(product): add Product entity

---
+++ src/main/java/com/raptor/ordersystem/entity/Product.java
@@ -0,0 +1,25 @@
+package com.raptor.ordersystem.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_id")
+    private int productId;
+
+    @Size(min = 1, max = 100)
+    @NotNull
+    private String description;
+}
