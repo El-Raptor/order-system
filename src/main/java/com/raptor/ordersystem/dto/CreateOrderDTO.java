098f6b8effb3158704ad5a4615544c52537a3507
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-11-07 16:10:56 -0400

    feat(product): add DTOs for Order entity

---
+++ src/main/java/com/raptor/ordersystem/dto/CreateOrderDTO.java
@@ -0,0 +1,17 @@
+package com.raptor.ordersystem.dto;
+
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CreateOrderDTO {
+    private UserDTO user;
+    private LocalDateTime orderDate;
+    private List<OrderItemDTO> items;
+}
