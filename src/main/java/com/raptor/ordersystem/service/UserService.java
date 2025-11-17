f139047e32ec1980eeda0235dd06cc6a5d27953c
Felipe Salles Lopes <felipe28101997@gmail.com>
2025-11-17 12:28:22 -0400

    feat(user): add role permissions check in get user by ID method for security business logic

--- src/main/java/com/raptor/ordersystem/service/UserService.java
+++ src/main/java/com/raptor/ordersystem/service/UserService.java
@@ -2,10 +2,10 @@ package com.raptor.ordersystem.service;
 
 import com.raptor.ordersystem.dto.CreateUserDTO;
 import com.raptor.ordersystem.dto.UserDTO;
-import com.raptor.ordersystem.dto.UserSummaryDTO;
 import com.raptor.ordersystem.entity.User;
 import com.raptor.ordersystem.mapper.UserMapper;
 import com.raptor.ordersystem.repository.UserRepository;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -17,10 +17,18 @@ public class UserService {
         this.userRepo = userRepo;
     }
 
+    /**
+     * Find a user by its ID.
+     *
+     * @param id User id
+     * @return <code>User</code> User based on the given Id.
+     */
+    @PreAuthorize("#id == authentication.principal.id or hasRole('ADMIN')")
     public User findUserById(Integer id) {
         return userRepo.findById(id).orElse(null);
     }
 
+    //@PreAuthorize("#id == authentication.principal.id or hasRole('ADMIN')")
     public User findUserByEmail(String email) {
         return userRepo.findByEmail(email).orElse(null);
     }
